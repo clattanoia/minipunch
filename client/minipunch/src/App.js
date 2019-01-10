c3f7a55a2dc2be646fc6363849caefd87b5de089
Yuanchun Li <ycli@CNycli.local>
2019-01-10 15:06:08 +0800

    add proxy

--- client/minipunch/src/App.js
+++ client/minipunch/src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { title: '' }
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get('/api/getTitle')
+      this.setState({
+        title: res.data.title
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.title}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- server/minipunch/routes/index.js
+++ server/minipunch/routes/index.js
@@ -2,18 +2,18 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello minipunch!'
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+router.get('/getTitle', async (ctx, next) => {
+  ctx.body = {
+    title: 'Welcome to minipunch'
+  }
 })
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+router.get('/getContent', async (ctx, next) => {
+  ctx.body = 'To get started'
 })
 
 module.exports = router
