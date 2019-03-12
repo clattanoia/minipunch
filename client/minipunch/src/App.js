d1b2c7622a09c0ec4d0e1282134ecc5d3422a12e
Yuanchun Li <ycli@CNycli.local>
2019-03-12 17:24:15 +0800

    add apollo client

--- client/minipunch/src/App.js
+++ client/minipunch/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.scss'
-import axios from './util/api'
+// import axios from './util/api'
+
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+const GET_USER = gql`
+  query($id: ID!) {
+    user(id: $id) {
+      id
+      name
+      sex
+      age
+    }
+  }
+`
+
+const variables = { id: '5c812e8b386ad706fd6fc19f' }
 
 class App extends Component {
   constructor(props) {
@@ -10,32 +25,39 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    try {
-      const {
-        data: { user }
-      } = await axios({
-        url: '/graphql',
-        method: 'post',
-        data: {
-          operationName: 'getUser',
-          query: 'query getUser($id: ID!) { user(id: $id) { id name sex age } }',
-          variables: { id: '5c812e8b386ad706fd6fc19f' }
-        }
-      })
-      this.setState({
-        name: user.name
-      })
-    } catch (error) {
-      console.error(error)
-    }
+    // try {
+    //   const {
+    //     data: { user }
+    //   } = await axios({
+    //     url: '/graphql',
+    //     method: 'post',
+    //     data: {
+    //       operationName: 'getUser',
+    //       query: 'query getUser($id: ID!) { user(id: $id) { id name sex age } }',
+    //       variables: { id: '5c812e8b386ad706fd6fc19f' }
+    //     }
+    //   })
+    //   this.setState({
+    //     name: user.name
+    //   })
+    // } catch (error) {
+    //   console.error(error)
+    // }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.name}</h1>
+          {/* <h1 className="App-title">{this.state.name}</h1> */}
+
+          <Query query={GET_USER} variables={variables}>
+            {({ loading, error, data: { user } }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+              return <h1 className="App-title">{user.name}</h1>
+            }}
+          </Query>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- client/minipunch/src/index.js
+++ client/minipunch/src/index.js
@@ -4,7 +4,27 @@ import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+
+const link = createHttpLink({
+  uri: 'http://127.0.0.1:8000/graphql',
+  credentials: 'same-origin'
+})
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
